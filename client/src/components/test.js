5c3fdb81186ea5dcbd6bdec36c37f7e9992bc302
jtagaca <jtagaca@csub.edu>
2021-04-11 20:48:04 -0700

    added reusable components for frontend building

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,19 +1,19 @@
 import React from "react";
-import InputGroup from 'react-bootstrap/InputGroup'
+import InputGroup from "react-bootstrap/InputGroup";
 import Carousel from "react-bootstrap/Carousel";
-import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
-import FormControl from 'react-bootstrap/FormControl'
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
 function test(props) {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="d-block w-100  "
             src="holder.js/800x400?text=First slide&bg=373940"
             alt="First slide"
           />
@@ -59,17 +59,52 @@ function test(props) {
           </Col>
         </Row>
       </Form>
+      <h1> GPA finder... </h1>
+      <Form.Control as="select">
+        <option>Default select</option>
+        <option>Default select</option>
+        <option>Default select</option>
+      </Form.Control>
 
+      <h1> Find a student </h1>
       <InputGroup className="mb-3">
         <FormControl
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
+          placeholder="Search By Student ID"
           aria-describedby="basic-addon2"
         />
         <InputGroup.Append>
-          <Button variant="outline-secondary">Button</Button>
+          <Button variant="outline-secondary">Search</Button>
         </InputGroup.Append>
       </InputGroup>
+      <h1> Find the Faculties </h1>
+      <Form.Control as="select">
+        <option>Department </option>
+        <option>Department </option>
+        <option>Department </option>
+      </Form.Control>
+      <Form>
+        <Form.Group controlId="formBasicRange">
+          <Form.Label>GPA range</Form.Label>
+          <Form.Control type="range" />
+        </Form.Group>
+      </Form>
+
+      <Form>
+        <Form.Row>
+          <Col xs={7}>
+            <Form.Control placeholder="Faculty Name" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Student Id" />
+          </Col>
+          {/* <Col>
+            <Form.Control placeholder="" />
+          </Col> */}
+        </Form.Row>
+      </Form>
+
+   
+      <Button variant="dark">Find</Button>{' '}
     </div>
   );
 }
