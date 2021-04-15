943f534a0b1d888a323b1bf10ce66299663c9dd8
jtagaca <jtagaca@csub.edu>
2021-04-14 21:44:30 -0700

    started to make the user page

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -8,13 +8,63 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl";
 import "./component.css";
+import { useState } from "react";
+import Axios from "axios";
 
-function test(props) {
+function Test(props) {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [newWage, setNewWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post("/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
   return (
     <div>
+
+    
+ 
      
+     <Form>
+        <Row>
+          <Col>
+            <Form.Control placeholder="First name" />
+          </Col>
+          <Col>
+            <Form.Control placeholder="Last name" />
+          </Col>
+        </Row>
+      </Form>
+   
+      <Button variant="dark">Find</Button>{' '}
+      
     </div>
   );
 }
 
-export default test;
+export default Test;
