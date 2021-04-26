3e7d82c212042576ba1a73d6c9f1b31831b22e77
jtagaca <jtagaca@csub.edu>
2021-04-25 21:55:02 -0700

    new stuff

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -96,10 +96,19 @@ function Test(props) {
       <div className="circle-wrapper">
         <div className="warning circle"></div>
         <div className="icon">
-        <Buttond variant="light">View all Courses</Buttond>{" "}
-
+          <Buttond variant="light">View all Courses</Buttond>{" "}
           <i classname="fa fa-exclamation"></i>
         </div>
+      </div>
+      <div className="cloud front">
+        <span className="left-front"></span>
+        <span className="right-front"></span>
+      </div>
+      <span className="sun sunshine"></span>
+      <span className="sun"></span>
+      <div className="cloud back">
+        <span className="left-back"></span>
+        <span className="right-back"></span>
       </div>
     </div>
   );
