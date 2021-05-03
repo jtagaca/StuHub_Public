99c9ce77dac80dfdd9bbc5d25b8d73235958be9a
jtagaca <jtagaca@csub.edu>
2021-05-03 16:26:12 -0700

    new changes

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,146 +1,79 @@
 import React from "react";
-import InputGroup from "react-bootstrap/InputGroup";
-import Carousel from "react-bootstrap/Carousel";
-import FormsB from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
+import { Button, ButtonGroup, Fab, Form, TextField } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 import Buttond from "react-bootstrap/Button";
-import "./component.scss";
-import { useState } from "react";
-import Axios from "axios";
-import Moment from "react-moment";
-import "moment-timezone";
-import Clock from "react-live-clock";
-import "./animate.scss";
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  FormHelperText,
-  Button,
-} from "@material-ui/core";
-
-function Test(props) {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [country, setCountry] = useState("");
-  const [position, setPosition] = useState("");
-  const [wage, setWage] = useState(0);
-
-  const [newWage, setNewWage] = useState(0);
-
-  const [employeeList, setEmployeeList] = useState([]);
-
-  const addEmployee = () => {
-    Axios.post("/create", {
-      name: name,
-      age: age,
-      country: country,
-      position: position,
-      wage: wage,
-    }).then(() => {
-      setEmployeeList([
-        ...employeeList,
-        {
-          name: name,
-          age: age,
-          country: country,
-          position: position,
-          wage: wage,
-        },
-      ]);
-    });
-  };
-  var ReactFitText = require("react-fittext");
+import RemoveIcon from "@material-ui/icons/Remove";
+import "./gg.css";
+function test() {
   return (
     <div>
-      <div className="float-start my-0">
-        <div className="cloud front">
-          <span className="left-front"></span>
-          <span className="right-front"></span>
+      <div className="uicontainer bgcoloradmin defaultspacing">
+        {/* how do we use the classes that are in bootstrap in here like rounded */}
+        <div>
+          <div className=" float-left spacing1 ">
+            {/* should be float left */}
+            <div className="float-left  ">
+              <h1>hello there this is for forms</h1>
+            </div>
+            <div className="float-none">{/* this is for removing user */}</div>
+          </div>
+          <div className="float-left  spacing2">
+            <Fab color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+
+            <div className="float-none">
+              <Fab color="primary" aria-label="add">
+                <RemoveIcon />
+              </Fab>
+            </div>
+          </div>
         </div>
-        <span className="sun sunshine"></span>
-        <span className="sun"></span>
-        <div className="cloud back">
-          <span className="left-back"></span>
-          <span className="right-back"></span>
+        <div className="margin:5%;">
+          <div className="float-left mx-2">
+            <form>
+              <TextField id="standard-basic" label="First Name" />
+            </form>
+          </div>
+          <div className="float-left">
+            <form>
+              <TextField id="standard-basic" label="Last Name" />
+            </form>
+          </div>
+          <div>
+            <Button variant="contained" color="primary">
+              Search
+            </Button>
+          </div>
         </div>
       </div>
-      <div>
-        <div className="circle-wrapper">
-          <div className="warning circle"></div>
-          <div className="icon">
-            <Buttond variant="light">View all Courses</Buttond>{" "}
-            <i classname="fa fa-exclamation"></i>
-          </div>
-        </div>
-      </div>
-      <div className="uicontainer mt-5">
-        <div className="container-fluid mt-3">
-          <Row>
-            <Col lg={12}>
-              <h1>
-                <Clock format={"h:mm:ssa"} interval={1000} ticking={true} />
-              </h1>
-            </Col>
-          </Row>
-        </div>
-        <div className="mx-5">
-          <Row className="container-fluid mx-0" >
-            <Col lg={6} className="border bordercolor ">
-              <h1>Welcome</h1>
-              <h2>$First name $last Name</h2>
-            </Col>
 
-            <Col lg={6} className="border bordercolor">
-              <div>
-                <h1>To the University of CSA, Bakersfield</h1>
-              </div>
-            </Col>
-          </Row>
-        </div>
+      <div className="uicontainer">
+        <h1>Actions</h1>
         <div>
-          <div className="borderhello bordercolor">
-            <div className="col align-items-center">
-              <h1>Hello there</h1>
-            </div>
+          <h1> Common Queries</h1>
+          <ButtonGroup
+            variant="contained"
+            color="primary"
+            aria-label="contained primary button group"
+          >
+            <Button>Select the student ID with the highest GPA</Button>
+            <Button>Show courses that are taken by all students</Button>
+            <Button>List all the courses that a student=name has taken</Button>
+          </ButtonGroup>
 
-            <div>
-              <Row>
-                <Col xl={12} className="border">
-                  <h1>Student ID: $1000001</h1>
-                </Col>
-              </Row>
-            </div>
-          </div>
+          {/* <ButtonGroup
+            variant="contained"
+            color="primary"
+            aria-label="contained primary button group"
+          >
+            <Button>Select the student ID with the highest GPA</Button>
+            <Button>Show courses that are taken by all students</Button>
+            <Button>List all the courses that a student=name has taken</Button>
+          </ButtonGroup> */}
         </div>
-        <Row></Row>
-        <Buttond variant="light">View all Courses</Buttond>{" "}
-        <Buttond variant="light">View GPA</Buttond>{" "}
-        <Buttond variant="light">
-          Show letter Grade every student got in the current semester
-        </Buttond>{" "}
-        {/* <div className="circle-wrapper">
-        <div className="success circle"></div>
-            <div className="icon">
-               <div className="fa fa-check"></div>
-            </div>
-        <div>
-      </div> */}
-        {/* <div className="gradient-border">Rainbow border</div> */}
-        {/* <div className="box">Rainbow border</div> */}
-        {/* <div className="background-img">
-        <div className="box">
-       
-          <div className="content">
-            <h2>My animated Border </h2>
-          </div>
-        </div>
-      </div> */}
       </div>
     </div>
   );
 }
-
-export default Test;
+export default test;
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-
+import {
+    BrowserRouter
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+ReactDOM.render( <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter>,
     document.getElementById("root")
 );
 
