0a8e9dfe48fe5f44d3ebc60e93ca3707c179dc3c
ppark <patparkk@gmail.com>
2021-05-04 15:43:31 -0700

    added react-tabs package

--- client/src/components/ptest.js
+++ client/src/components/ptest.js
@@ -8,11 +8,23 @@ import { Dropdown, Row, Col } from "react-bootstrap";
 import Formb from "react-bootstrap/Form";
 import Carousel from "react-bootstrap/Carousel";
 import "./gg.css";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 function ptest() {
   return (
-    <div>
-        TESTING123
-    </div>
+    <Tabs>
+        <TabList>
+            <Tab>title 1</Tab>
+            <Tab>title 2</Tab>
+        </TabList>
+        <TabPanel>
+            <h2>title 1 junk</h2>
+        </TabPanel>
+        <TabPanel>
+            <h2>title 2 junk</h2>
+        </TabPanel>
+    </Tabs>
   );
 }
 export default ptest;
