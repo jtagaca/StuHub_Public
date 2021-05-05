34cf08d78a27974eb95e6f204fb102d650482afd
ppark <patparkk@gmail.com>
2021-05-05 12:06:06 -0700

    added prototype tabs to ptest

--- client/src/components/ptest.js
+++ client/src/components/ptest.js
@@ -13,18 +13,57 @@ import 'react-tabs/style/react-tabs.css';
 
 function ptest() {
   return (
+    <div>
     <Tabs>
         <TabList>
-            <Tab>title 1</Tab>
-            <Tab>title 2</Tab>
+            <Tab>Find User</Tab>
+            <Tab>Add User</Tab>
+            <Tab>Update User</Tab>
+            <Tab>Delete User</Tab>
         </TabList>
         <TabPanel>
             <h2>title 1 junk</h2>
         </TabPanel>
         <TabPanel>
+        <h1>Enter New User Profile</h1>
+              <Formb>
+                <Row>
+                  <Col>
+                    <Formb.Control placeholder="Student ID" />
+                  </Col>
+                  <Col>
+                    <Formb.Control placeholder="First Name" />
+                  </Col>
+                  <Col>
+                    <Formb.Control placeholder="Last Name" />
+                  </Col>
+                  <Col>
+                    <Formb.Control placeholder="Password" />
+                  </Col>
+                  <Col>
+                    <Formb.Control placeholder="Email" />
+                  </Col>{" "}
+                  <Col>
+                    <Formb.Control placeholder="Phone" />
+                  </Col>{" "}
+                </Row>
+              </Formb>
+        </TabPanel>
+        <TabPanel>
             <h2>title 2 junk</h2>
         </TabPanel>
+        <TabPanel>
+        <h1>Delete User Account</h1>
+              <Formb>
+                <Row>
+                  <Col>
+                    <Formb.Control placeholder="Student ID" />
+                  </Col>
+                </Row>
+              </Formb>
+        </TabPanel>
     </Tabs>
+    </div>
   );
 }
 export default ptest;
