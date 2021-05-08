80733d21b7fb62256fa7f37ebb75ba4f32d18ae4
ppark <patparkk@gmail.com>
2021-05-08 14:59:07 -0700

    ptest2 testing

--- client/src/components/ptest2.js
+++ client/src/components/ptest2.js
@@ -13,8 +13,49 @@ import 'react-tabs/style/react-tabs.css';
 import "../index.css";
 
 function ptest2() {
-  return (
-    <h1>Hello</h1>
+  return ( 
+    <body> 
+        <div id = "boxes">
+            <h1 style = {{color:'green',
+                textalign:'center'}}>GeeksforGeeks</h1>
+              
+            <div id = "leftbox" style = {{float:'left',
+                background:'red',
+                width:'25%',
+                height:'280px'}}>
+                <h2>Learn:</h2>
+                It is a good platform to learn programming.
+                It is an educational website. Prepare for
+                the Recruitment drive  of product based 
+                companies like Microsoft, Amazon, Adobe 
+                etc with a free online placement preparation 
+                course.
+            </div> 
+              
+            <div id = "middlebox" style = {{float:'left',
+                background:'green',
+                width:'50%',
+                height:'280px'}}>
+                <h2>GeeksforGeeks:</h2>
+                The course focuses on various MCQ's & Coding 
+                question likely to be asked in the interviews
+                & make your upcoming placement season efficient
+                and successful.
+            </div>
+              
+            <div id = "rightbox" style = {{float:'right',
+                background:'blue',
+                width:'25%',
+                height:'280px'}}>
+                <h2>Contribute:</h2>
+                Any geeks can help other geeks by writing
+                articles on the GeeksforGeeks, publishing
+                articles follow few steps that are Articles
+                that need little modification/improvement
+                from reviewers are published first.
+            </div>
+        </div>
+    </body> 
   );
 }
 export default ptest2;
