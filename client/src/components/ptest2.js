2009053439467b9bf69198d38eabd1f3686d46cd
ppark <patparkk@gmail.com>
2021-05-08 15:03:45 -0700

    more ptest2 testing

--- client/src/components/ptest2.js
+++ client/src/components/ptest2.js
@@ -14,48 +14,34 @@ import "../index.css";
 
 function ptest2() {
   return ( 
-    <body> 
-        <div id = "boxes">
+      <div>
             <h1 style = {{color:'green',
-                textalign:'center'}}>GeeksforGeeks</h1>
+                textalign:'center'}}>module testing</h1>
               
             <div id = "leftbox" style = {{float:'left',
                 background:'red',
                 width:'25%',
                 height:'280px'}}>
-                <h2>Learn:</h2>
-                It is a good platform to learn programming.
-                It is an educational website. Prepare for
-                the Recruitment drive  of product based 
-                companies like Microsoft, Amazon, Adobe 
-                etc with a free online placement preparation 
-                course.
+                <h2>module1</h2>
+                test1
             </div> 
               
             <div id = "middlebox" style = {{float:'left',
                 background:'green',
                 width:'50%',
                 height:'280px'}}>
-                <h2>GeeksforGeeks:</h2>
-                The course focuses on various MCQ's & Coding 
-                question likely to be asked in the interviews
-                & make your upcoming placement season efficient
-                and successful.
+                <h2>module2</h2>
+                test2
             </div>
               
             <div id = "rightbox" style = {{float:'right',
                 background:'blue',
                 width:'25%',
                 height:'280px'}}>
-                <h2>Contribute:</h2>
-                Any geeks can help other geeks by writing
-                articles on the GeeksforGeeks, publishing
-                articles follow few steps that are Articles
-                that need little modification/improvement
-                from reviewers are published first.
+                <h2>module3</h2>
+                test3
             </div>
         </div>
-    </body> 
   );
 }
 export default ptest2;
