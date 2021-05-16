d7056f36046118380b64eda2bf58528c14348af2
ElijahM300 <emorris2@csub.edu>
2021-05-15 18:02:06 -0700

    Fixed routing for etest

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import SignUp from "./components/signup.component";
 import Test from "./components/test";
 import User from "./components/user";
 import Ptest from "./components/ptest";
-import etest from "./components/etest";
+import Etest from "./components/etest";
 import ktest from "./components/ktest";
 import ptest2 from "./components/ptest2";
 import Admin from "./components/admin";
@@ -84,7 +84,7 @@ function App() {
           <Route path="/test" component={Test} />
           <Route path="/Profile" component={User} />
           <Route path="/ptest" component={Ptest} />
-          <Route path="/etest" component={etest} />
+          <Route path="/etest" component={Etest} />
           <Route path="/ktest" component={ktest} />
           <Route path="/ptest2" component={ptest2} />
           <Route path="/addusertemp" component={Adduser} />
