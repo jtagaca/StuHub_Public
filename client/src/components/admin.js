06b5591bdd52aa4c086308de05993731e14e7067
jtagaca <jtagaca@csub.edu>
2021-05-18 16:48:35 -0700

    modal table

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -628,39 +628,26 @@ function Admin() {
             </div>
 
             <div class="modal-body">
-              <p>
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </p>
+              {allstudentlist &&
+                allstudentlist.map((student) => {
+                  return (
+                    <tbody key={student.Login_ID} style={{ textAlign: "left" }}>
+                      <tr>
+                        <td>
+                          {student.First_Name} {student.Last_Name}
+                        </td>
+                        <td>{student.Login_ID}</td>
+                        <td>{student.Email}</td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
             </div>
           </div>
         </div>
       </div>
 
-      <div>
-        {allstudentlist &&
-          allstudentlist.map((student) => {
-            return (
-              <tbody key={student.Login_ID} style={{ textAlign: "left" }}>
-                <tr>
-                  <td>
-                    {student.First_Name} {student.Last_Name}
-                  </td>
-                  <td>{student.Login_ID}</td>
-                  <td>{student.Email}</td>
-                </tr>
-              </tbody>
-            );
-          })}
-      </div>
+      <div></div>
     </div>
   );
 }
