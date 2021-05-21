0b18bd7cb820af98a9853dfa6e8bfd64eda07126
jtagaca <jtagaca@csub.edu>
2021-05-21 16:27:33 -0700

    refactored

---
+++ client/src/Functions/RegExValidation.js
@@ -0,0 +1,6 @@
+const regEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+const regPassword =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const regInt = /\d/;
+const regChar = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+const regLetter = /[a-zA-Z]/;
